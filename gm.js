165c319c057f6d4c4c54dc813f25c0f26680429b
Tanya Ly <tanya.lytovchenko@gmail.com>
2019-03-19 14:17:15 +0100

    first commit

---
+++ gm.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $(".city-filter").on("click", function (ev) {
+        filterCity = $(this).text();
+        updateAddresses();
+    });
+});
+
+let addresses = [];
+let filterCity = "All";
+
+function updateAddresses() {
+    $('#nav-tabContent').empty();
+    let filtered = addresses.apartments.filter(function (addr) {
+        return filterCity == "All" || addr.city == filterCity;
+    });
+    console.log(filtered);
+    for (let f in filtered) {
+        let newDiv = $("<div class='card'></div>").html(filtered[f].description + '<br/>' +
+            filtered[f].address + "<br/>" + "Bedrooms: " + filtered[f].bedrooms + " / " +
+            "Neighborhood: " + filtered[f].neighborhood);
+        let price = $("<div class='price'></div>").text(filtered[f].price);
+        newDiv.prepend(price);
+
+        // make description in the right side
+        $('#nav-tabContent').append(newDiv);
+    }
+};
+
+$.ajax('https://api.myjson.com/bins/2sadq?pretty=1', {
+    success: function (data) {
+        addresses = data;
+        updateAddresses();
+    },
+    error: function () {
+        $('#notification-bar').text('An error occurred');
+    }
+});
